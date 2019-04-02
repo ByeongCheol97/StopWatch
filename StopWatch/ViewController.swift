4089e55b64e7eabbaf8027f63729dd60cf448105
D7703_13 <d7703_13@d7703-13ui-imac.local>
2019-04-02 16:10:28 +0900

    commit

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -1,20 +1,31 @@
-//
-//  ViewController.swift
-//  StopWatch
-//
-//  Created by D7703_13 on 2019. 4. 2..
-//  Copyright © 2019년 Personal Team. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    var count = 0
+    var myTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    func updateTime() {
+        count = count + 1
+        let min = count / 60 / 100
+        let sec = (count - (min * 60 * 100)) / 100
+        let msec = count - (min * 60 * 100) - (sec * 100)
+        timeLabel.text = String(format: "%02d:%02d:%02d", min, sec, msec)
+    }
+    @IBAction func playbtn(_ sender: Any) {
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1/100, repeats: true, block: {(myTimer) in self.updateTime()
+        })
+    }
+    @IBAction func pausebtn(_ sender: Any) {
+        myTimer.invalidate()
+    }
+    @IBAction func stopbtn(_ sender: Any) {
+        myTimer.invalidate()
+        count = 0
+        timeLabel.text = "00:00:00"
+    }
 }
-
